0b91bb1388a645b38c7e4d60cb77be54793ad723
Alexander Sylvanus <alexsylvanus@gmail.com>
2018-07-31 17:35:28 -0400

    First Commit

---
+++ PracticeCode/template.c
@@ -0,0 +1,26 @@
+/*	File:			template.c
+	Location:		/PracticeCode/template.c
+	Time:			6:56:01 PM
+	Date:			Jul 24, 2018
+	Author:			alexs
+	Description:	Enter description
+*/
+
+// Includes
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+// Defines
+
+// Variable Type Definitions
+
+// Struct Type Definitions
+
+// Enum Type Definitions
+
+// Global Variables
+
+// Functions
+
+
+
